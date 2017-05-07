ad2412c48961a8eeaeaba57e553e58c2c865ce83
ktsn <ktsn55@gmail.com>
2017-05-07 23:13:02 +0900

    Allow glob to specify sources

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import * as minimist from 'minimist'
+import * as glob from 'glob'
 import { run } from '../main'
 
 const {
   _: [source]
 } = minimist(process.argv.slice(2))
 
-run([source])
+run(glob.sync(source))
